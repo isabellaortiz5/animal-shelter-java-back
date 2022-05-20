c83e4c1b84fcf6ca66e2b1f51c0b360f78709978
isabellaortiz5 <isabellaortiz5@yahoo.com>
2022-05-19 21:47:23 -0500

    first edits

---
+++ src/test/java/com/shelter/animalback/contract/api/animal/AnimalTest.java
@@ -0,0 +1,61 @@
+
+package com.shelter.animalback.contract.api.animal;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
+import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shelter.animalback.controller.AnimalController;
+import com.shelter.animalback.domain.Animal;
+import com.shelter.animalback.service.interfaces.AnimalService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@PactBroker(url = "${PACT_BROKER_BASE_URL}", authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
+@Provider("AnimalShelterBack")
+@ExtendWith(MockitoExtension.class)
+public class AnimalTest {
+    @Mock
+    private AnimalService animalService;
+    @InjectMocks
+    private AnimalController animalController;
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    public void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    @BeforeEach
+    public void changeContext(PactVerificationContext context) {
+        MockMvcTestTarget testTarget = new MockMvcTestTarget();
+        testTarget.setControllers(animalController);
+        context.setTarget(testTarget);
+        System.setProperty("pact.verifier.publishResults", "true");
+    }
+
+    @State("has animals")
+    public void listAnimals() {
+        Animal animal = new Animal();
+        animal.setName("Bigotes");
+        animal.setBreed("Siames");
+        animal.setGender("Male");
+        animal.setVaccinated(false);
+        List<Animal> animals = new ArrayList<Animal>();
+        animals.add(animal);
+        Mockito.when(animalService.getAll()).thenReturn(animals);
+    }
+
+}
