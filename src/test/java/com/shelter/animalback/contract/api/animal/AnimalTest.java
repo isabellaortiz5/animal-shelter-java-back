55966d844052325ec7b172fc3064ca2b4be1da17
isabellaortiz5 <isabellaortiz5@yahoo.com>
2022-05-21 23:12:50 -0500

    Contract tests

--- src/test/java/com/shelter/animalback/contract/api/animal/AnimalTest.java
+++ src/test/java/com/shelter/animalback/contract/api/animal/AnimalTest.java
@@ -9,6 +9,8 @@ import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 
+import static org.mockito.ArgumentMatchers.any;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,15 +25,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 @PactBroker(url = "${PACT_BROKER_BASE_URL}", authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
 @Provider("AnimalShelterBack")
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class AnimalTest {
     @Mock
     private AnimalService animalService;
     @InjectMocks
     private AnimalController animalController;
+
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
     public void pactVerificationTestTemplate(PactVerificationContext context) {
@@ -56,6 +62,44 @@ public class AnimalTest {
         List<Animal> animals = new ArrayList<Animal>();
         animals.add(animal);
         Mockito.when(animalService.getAll()).thenReturn(animals);
+    }
+
+    @State("has an animal")
+    public void listAnimalByName() {
+        Animal animal = new Animal();
+        animal.setName("Joy");
+        animal.setBreed("Criollo");
+        animal.setGender("Male");
+        animal.setVaccinated(true);
+        Mockito.when(animalService.get("Joy")).thenReturn(animal);
+    }
+
+    @State("adds an animals")
+    public void addAnimal() {
+        Animal animal = new Animal();
+        animal.setName("Joy");
+        animal.setBreed("Bengali");
+        animal.setGender("Male");
+        animal.setVaccinated(true);
+        Mockito.lenient().when(animalService.save(any(Animal.class))).thenReturn(animal);
+    }
+
+    @State("update animasl")
+    public void updateAnimal() {
+        Animal animal = new Animal();
+        animal.setId(1);
+        animal.setName("Joy");
+        animal.setBreed("Criollo");
+        animal.setGender("Male");
+        animal.setVaccinated(true);
+        String[] vaccines = { "rabia" };
+        animal.setVaccines(vaccines);
+        Mockito.when(animalService.replace("Joy", animal)).thenReturn(animal);
+    }
+
+    @State("deletes animals")
+    public void deleteAnimalByName() {
+        Mockito.doNothing().when(animalService).delete("Joy");
     }
 
 }
